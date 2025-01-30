79b70bb82e8366ef8deaf679a7fa5a4bdd3428b7
JayySaucee <jorgealejandre.piki@gmail.com>
2025-01-30 13:03:51 -0800

    Initialized tailwindcss v3 instead of v4 due to issues arising with v4. Need to work on design further

--- portfoliosite/src/components/navbar.js
+++ portfoliosite/src/components/navbar.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <nav className="navbar">
-        <ul>
-            <li>
-            <Link to="/">Home</Link>
-            </li>
-            <li>
-            <Link to="/about">About</Link>
-            </li>
-            <li>
-            <Link to="/contact">Contact</Link>
-            </li>
-        </ul>
+        <nav className="bg-gray-800">
+            <ul className="flex space-x-4 p-4">
+                <li>
+                    <Link to="/" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md">Contact</Link>
+                </li>
+            </ul>
         </nav>
     );
-    }
+}
 
 export default Navbar;
